541e43f2587ec7b5d5e48f511fff589728c99ce1
Dominic Ewing <ewingdj@uw.edu>
2021-02-23 21:06:23 -0800

    Define initial rental system interface

---
+++ src/rental_system.cpp
@@ -0,0 +1,90 @@
+/**
+ * @file rental_system.cpp
+ * @author Dominic Ewing
+ */
+
+#include <utility>	// std::move
+
+#include "rental_system.hpp"
+#include "rental_system_error.hpp"
+
+RentalSystem(Inventory inv, Clientele clients, std::unique_ptr<Display> disp)
+	: inventory{std::move(inv)},
+	customers{std::move(clients)},
+	display{std::move(disp)}
+{
+}
+
+/**
+ * Rent an inventory item to a customer.
+ *
+ * @param command The command object containing the necessary information to
+ *                carry out the command.
+ */
+void RentalSystem::borrow(Transaction command)
+{
+	try
+	{
+		auto& customer = customers.findCustomer(command.customerID());
+		auto& item = inventory.borrow(command.item());
+	}
+	catch (const RentalSystemError& error)
+	{
+		display->reportError(error);
+		return;
+	}
+
+	customer.borrow(std::move(command), item);
+}
+
+/**
+ * Return a borrowed item from a customer to the inventory.
+ *
+ * @param command The command object containing the necessary information to
+ *                carry out the command.
+ */
+void RentalSystem::restock(Transaction command)
+{
+	try
+	{
+		auto& customer = customers.findCustomer(command.customerID());
+		auto& item = customer.restock(command.item());
+
+		// What is the API to roll back the customer.restock() should
+		// inventory.restock() fail?
+		inventory.restock(item);
+	}
+	catch (const RentalSystemError& error)
+	{
+		display->reportError(error);
+		return;
+	}
+}
+
+/**
+ * Display the rental system inventory to the user.
+ */
+void RentalSystem::inventory()
+{
+	display->display(inventory.inventory());
+}
+
+/**
+ * Display the customer's transaction history to the user.
+ *
+ * @param customerID The id of the desired customer.
+ */
+void RentalSystem::history(const CustomerID& customerID)
+{
+	try
+	{
+		auto& customer = customers.findCustomer(customerID);
+	}
+	catch (const RentalSystemError& error)
+	{
+		display->reportError(error);
+		return;
+	}
+
+	display->display(customer.history());
+}
---
+++ src/rental_system.hpp
@@ -0,0 +1,39 @@
+/**
+ * @file rental_system.hpp
+ * @author Dominic Ewing
+ */
+
+#ifndef RENTAL_SYSTEM_RENTAL_SYSTEM_HPP
+#define RENTAL_SYSTEM_RENTAL_SYSTEM_HPP
+
+#include <memory>	// Use smart pointers for owned memeory.
+
+#include "customer_id.hpp"
+#include "transaction.hpp"
+#include "display.hpp"
+
+/**
+ * Main rental system class coordinating the inventory and customer data stores,
+ * output display, and providing the command interface.
+ */
+class RentalSystem
+{
+public:
+	RentalSystem(Inventory inv, Clientele clients,
+	             std::unique_ptr<Display> disp);
+
+	void borrow(Transaction command);
+	void restock(Transaction command);
+	void inventory();
+	void history(const CustomerID& customerID);
+
+private:
+	Inventory inventory;
+	Clientele customers;
+
+	// Display is abstract. Use unique_ptr to allow polymorphism while still
+	// having memory safety.
+	std::unique_ptr<Display> display;
+};
+
+#endif
