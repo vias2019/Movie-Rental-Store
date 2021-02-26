f60b6cd08232856a1476a78d9dd63e4151b943c2
SpinningSandwich <gyro567@gmail.com>
2021-02-25 21:02:35 -0800

    Define initial item interface

---
+++ src/item.h
@@ -0,0 +1,21 @@
+/**
+ * @file item.h
+ * @author Austin Arlitt
+ */
+
+#ifndef ITEM_H
+#define ITEM_H
+
+// includes TBD
+
+/**
+ * Abstract base class for items stocked in the inventory
+ */
+class Item {
+public:
+    virtual void addStock(int) = 0;     // adds total stock to inventory
+    virtual void borrow() = 0;          // attempts to check out item for a customer, decreasing stock
+    virtual void restock() = 0;         // returns a borrowed item, increasing stock
+};
+
+#endif
