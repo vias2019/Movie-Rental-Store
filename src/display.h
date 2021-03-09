7e0f0c35a036107a12eedeac5982bd37237b5fbf
Dominic Ewing <ewingdj@uw.edu>
2021-03-09 15:55:11 -0800

    Include vector in display.h

--- src/display.h
+++ src/display.h
@@ -6,6 +6,8 @@
 #ifndef DISPLAY_H
 #define DISPLAY_H
 
+#include <vector>
+
 #include "item.h"
 #include "rental_system_error.h"
 #include "transaction.h"
@@ -15,8 +17,8 @@
  */
 class Display
 {
-	virtual void display(const std::vector<Item>& items) const = 0;
-	virtual void display(const std::vector<Transaction>& commands) const = 0;
+	virtual void display(const std::vector<Item&>& items) const = 0;
+	virtual void display(const std::vector<Transaction&>& commands) const = 0;
 	virtual void displayError(const RentalSystemError& error) const = 0;
 };
 
