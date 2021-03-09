91faafba964efcead8c007d9b2cb4929743ba593
Victoria Grishkina <vias2010@gmail.com>
2021-03-09 12:54:01 -0800

    Added tests on main.cpp file

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@ using namespace std;
 
 int main()
 {
+   //======================= Viktoriya's tests======================================================
    HashTable M;
    M.insert(6, "Vika", "Gri");
    Customer v = M.findCustomer(3);
@@ -27,16 +28,7 @@ int main()
    M.printTransactions(6);
  
    //M.deleteData(3);
+   //=================================================================================================
    std::cout << "Hello World!\n";
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
