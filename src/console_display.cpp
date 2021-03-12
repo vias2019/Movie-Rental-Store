3e2d8a7a037e486bbf43a1b7924142f5e773342c
Dominic Ewing <ewingdj@uw.edu>
2021-03-11 16:53:50 -0800

    Implement ConsoleDisplay

---
+++ src/console_display.cpp
@@ -0,0 +1,47 @@
+/**
+ * @file console_display.cpp
+ * @author Dominic Ewing
+ */
+
+#include <algorithm>	// std::for_each
+#include <iostream>	// std::cout and std::cerr
+
+#include "console_display.h"
+
+/**
+ * Display the rental system inventory to stdout.
+ *
+ * @param items The list of items in the rental sytsem inventory in sorted order.
+ */
+void ConsoleDisplay::displayInventory(const std::vector<std::shared_ptr<Item>>& items) const
+{
+	std::cout << "Current Rental System Inventory:\n";
+	std::for_each(std::cbegin(items), std::cend(items), [](const auto& item) {
+		std::cout << item->toString() << '\n';
+	});
+}
+
+/**
+ * Display a customer's transaction history to stdout.
+ *
+ * @param customerID The id number of the customer.
+ * @param transactions The list of transactions in the customer's history.
+ */
+void ConsoleDisplay::displayHistory(int customerID, std::vector<Customer::History> transactions) const
+{
+	std::cout << "Transaction history for customer " << customerID << ":\n";
+	std::for_each(std::cbegin(transactions), std::cend(transactions), [](const auto& transaction) {
+		std::cout << *transaction.movie << '\n';
+	});
+}
+
+/**
+ * Display an error message to stderr.
+ *
+ * @param error The error object whose message will be displayed to the user.
+ */
+void ConsoleDisplay::displayError(const std::runtime_error& error) const
+{
+	std::cerr << "ERROR: ";
+	std::cerr << error.what() << '\n';
+}
