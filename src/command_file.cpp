1a4939f315f20e677c9ac2098b690da76a01c36d
Dominic Ewing <ewingdj@uw.edu>
2021-03-16 16:28:59 -0700

    Restyle CommandFile

--- src/command_file.cpp
+++ src/command_file.cpp
@@ -3,7 +3,7 @@
  * @author Dominic Ewing
  */
 
-#include <fstream>
+#include <fstream>  // ifstream
 
 #include "borrow_command.h"
 #include "classic_movie.h"
@@ -15,166 +15,184 @@
 #include "restock_command.h"
 
 /**
- * Construct a command file object.
+ * Constructor - Construct a command file object.
  *
  * @param filePath Path to the command file.
  */
 CommandFile::CommandFile(std::string filePath)
-	: file_path{std::move(filePath)}
+    : file_path{std::move(filePath)}
 {
 }
 
 /**
- * Read the command file to produce a list of commands based on its contents.
+ * commands - Read the command file to produce a list of commands based on its
+ *            contents.
  *
  * @return A list of command objects read from the file.
  */
 std::vector<std::shared_ptr<Command>> CommandFile::commands()
 {
-	if (!parsed)
-	{
-		parseFile();
-	}
+    // Parse the file if there is no data in cache.
+    if (!parsed)
+    {
+        parseFile();
+    }
 
-	return file_commands;
+    return file_commands;
 }
 
 /**
- * Provide all the errors encountered while parsing the file.
+ * errors - Provide all the errors encountered while parsing the file.
  *
  * @return A list of all parsing errors.
  */
 std::vector<std::shared_ptr<std::runtime_error>> CommandFile::errors()
 {
-	if (!parsed)
-	{
-		parseFile();
-	}
+    // Parse the file if there is no data in cache.
+    if (!parsed)
+    {
+        parseFile();
+    }
 
-	return file_errors;
+    return file_errors;
 }
 
 /**
- * Parse the file and store its contents in the cache.
+ * parseFile - Parse the file and store its contents in the cache.
  */
 void CommandFile::parseFile()
 {
-	std::ifstream stream{file_path};
+    std::ifstream stream{file_path};
 
-	for (std::string line; std::getline(stream, line); )
-	{
-		std::smatch inventory_match;
-		std::smatch history_match;
-		std::smatch borrow_match;
-		std::smatch return_match;
+    for (std::string line; std::getline(stream, line); )
+    {
+        std::smatch inventory_match;
+        std::smatch history_match;
+        std::smatch borrow_match;
+        std::smatch return_match;
 
-		if (std::regex_search(line, inventory_match, inventory_pattern))
-		{
-			file_commands.emplace_back(
-				std::shared_ptr<Command>{new InventoryCommand{}});
-		}
-		else if (std::regex_search(line, history_match, history_pattern))
-		{
-			file_commands.emplace_back(
-				std::shared_ptr<Command>{new HistoryCommand{
-					stoi(history_match[1])
-			}});
-		}
-		else if (std::regex_search(line, borrow_match, borrow_pattern))
-		{
-			try
-			{
-				auto item = parseDVD(line);
-				file_commands.emplace_back(
-					std::shared_ptr<Command>{new BorrowCommand{
-						stoi(borrow_match[1]),
-						std::make_unique<DVD>(item)
-				}});
-			}
-			catch (const std::runtime_error& error)
-			{
-				file_errors.emplace_back(
-					std::make_shared<std::runtime_error>(
-						"[Invalid command] " + line
-				));
-			}
-		}
-		else if (std::regex_search(line, return_match, return_pattern))
-		{
-			try
-			{
-				auto item = parseDVD(line);
-				file_commands.emplace_back(
-					std::shared_ptr<Command>{new RestockCommand{
-						stoi(return_match[1]),
-						std::make_unique<DVD>(item)
-				}});
-			}
-			catch (const std::runtime_error& error)
-			{
-				file_errors.emplace_back(
-					std::make_shared<std::runtime_error>(
-						"[Invalid command] " + line
-				));
-			}
-		}
-		else
-		{
-			file_errors.emplace_back(
-				std::make_shared<std::runtime_error>(
-					"[Invalid command] " + line
-			));
-		}
-	}
+        // Match based on command type: inventory, history, borrow, or
+        // return
+        if (std::regex_search(line, inventory_match, inventory_pattern))
+        {
+            file_commands.emplace_back(
+                std::shared_ptr<Command>{
+                    new InventoryCommand{}
+            });
+        }
+        else if (std::regex_search(line, history_match, history_pattern))
+        {
+            file_commands.emplace_back(
+                std::shared_ptr<Command>{new HistoryCommand{
+                    stoi(history_match[1])
+            }});
+        }
+        else if (std::regex_search(line, borrow_match, borrow_pattern))
+        {
+            try
+            {
+                // Throws an error if there is no matching dvd
+                auto item = parseDVD(line);
+                file_commands.emplace_back(
+                    std::shared_ptr<Command>{new BorrowCommand{
+                        stoi(borrow_match[1]),
+                        std::make_unique<DVD>(item)
+                }});
+            }
+            catch (const std::runtime_error& error)
+            {
+                logError(line);
+            }
+        }
+        else if (std::regex_search(line, return_match, return_pattern))
+        {
+            try
+            {
+                // Throws an error if there is no matching dvd
+                auto item = parseDVD(line);
+                file_commands.emplace_back(
+                    std::shared_ptr<Command>{new RestockCommand{
+                        stoi(return_match[1]),
+                        std::make_unique<DVD>(item)
+                }});
+            }
+            catch (const std::runtime_error& error)
+            {
+                logError(line);
+            }
+        }
+        else
+        {
+            logError(line);
+        }
+    }
 
-	parsed = true;
+    // Indicate there is a valid cache
+    parsed = true;
 }
 
 /**
- * Parse a line in the file to build the involved DVD object.
+ * parseDVD - Parse a line in the file to build the involved DVD object.
  *
+ * This method fills in as much information as possible, but the command file
+ * does not contain all the movie information. Any missing fields are simply
+ * left as the empty string, or the value -1 to indicate missing data.
  *
  * @param line The line in the command file.
  * @return The matching DVD object.
- * @throws If this method fails to match a DVD, it will throw std::runtime_error
+ * @throws std::runtime_error if there is no matching DVD movie format
  */
 DVD CommandFile::parseDVD(const std::string& line)
 {
-		std::smatch comedy_match;
-		std::smatch drama_match;
-		std::smatch classic_match;
+    std::smatch comedy_match;
+    std::smatch drama_match;
+    std::smatch classic_match;
 
-		if (std::regex_search(line, comedy_match, comedy_pattern))
-		{
-			return DVD{std::shared_ptr<Movie>{new ComedyMovie(
-				"comedy",
-				"",
-				comedy_match[1],
-				stoi(comedy_match[2])
-			)}};
-		}
-		else if (std::regex_search(line, drama_match, drama_pattern))
-		{
-			return DVD{std::shared_ptr<Movie>{new DramaMovie(
-				"drama",
-				drama_match[1],
-				drama_match[2],
-				-1
-			)}};
-		}
-		else if (std::regex_search(line, classic_match, classic_pattern))
-		{
-			return DVD{std::shared_ptr<Movie>{new ClassicMovie(
-				"classic",
-				"",
-				"",
-				stoi(classic_match[2]),
-				stoi(classic_match[1]),
-				classic_match[3]
-			)}};
-		}
-		else
-		{
-			throw std::runtime_error("No matching DVD format");
-		}
+    // Match based on movie type: comedy, drama, or classic
+    if (std::regex_search(line, comedy_match, comedy_pattern))
+    {
+        return DVD{std::shared_ptr<Movie>{new ComedyMovie(
+            "comedy",               // .genre
+            "",                     // .director
+            comedy_match[1],        // .title
+            stoi(comedy_match[2])   // .releaseYear
+        )}};
+    }
+    else if (std::regex_search(line, drama_match, drama_pattern))
+    {
+        return DVD{std::shared_ptr<Movie>{new DramaMovie(
+            "drama",                // .genre
+            drama_match[1],         // .director
+            drama_match[2],         // .title
+            -1                      // .releaseYear
+        )}};
+    }
+    else if (std::regex_search(line, classic_match, classic_pattern))
+    {
+        return DVD{std::shared_ptr<Movie>{new ClassicMovie(
+            "classic",              // .genre
+            "",                     // .director
+            "",                     // .title
+            stoi(classic_match[2]), // .releaseYear
+            stoi(classic_match[1]), // .releaseMonth
+            classic_match[3]        // .actor
+        )}};
+    }
+    else
+    {
+        throw std::runtime_error("No matching DVD format");
+    }
+}
+
+/**
+ * logError - Add an error message to the list of parsing errors.
+ *
+ * @param line The input line where the error occurs.
+ */
+void CommandFile::logError(const std::string& line)
+{
+    file_errors.emplace_back(
+        std::make_shared<std::runtime_error>(
+            "[Invalid command] " + line
+    ));
 }
--- src/command_file.h
+++ src/command_file.h
@@ -6,11 +6,11 @@
 #ifndef COMMAND_FILE_H
 #define COMMAND_FILE_H
 
-#include <memory>
-#include <regex>
-#include <stdexcept>
-#include <string>
-#include <vector>
+#include <memory>   // shared_ptr, unique_ptr
+#include <regex>    // regex, regex_search, smatch
+#include <stdexcept>    // runtime_error
+#include <string>   // string
+#include <vector>   // vector
 
 #include "command.h"
 #include "dvd.h"
@@ -22,62 +22,69 @@
 class CommandFile
 {
 public:
-	explicit CommandFile(std::string filePath);
+    // Constructor
+    explicit CommandFile(std::string filePath);
 
-	std::vector<std::shared_ptr<Command>> commands();
-	std::vector<std::shared_ptr<std::runtime_error>> errors();
+    // Retrieve commands and errors
+    std::vector<std::shared_ptr<Command>> commands();
+    std::vector<std::shared_ptr<std::runtime_error>> errors();
 
 private:
-	std::string file_path;
+    std::string file_path;  // Path to the command file
 
-	// Cache from previously parsing the file. This prevents re-parsing
-	// for each separate call to commands() and errors().
-	bool parsed{false};
-	std::vector<std::shared_ptr<Command>> file_commands{};
-	std::vector<std::shared_ptr<std::runtime_error>> file_errors{};
+    // Cache from previously parsing the file. This prevents re-parsing
+    // for each separate call to commands() and errors().
+    bool parsed{false};
+    std::vector<std::shared_ptr<Command>> file_commands{};
+    std::vector<std::shared_ptr<std::runtime_error>> file_errors{};
 
-	// Helper methods for performing file parsing and updating cache.
-	void parseFile();
-	DVD parseDVD(const std::string& line);
+    // Helper methods for performing file parsing and updating cache.
+    void parseFile();
+    DVD parseDVD(const std::string& line);
+    void logError(const std::string& line);
 
-	const std::regex inventory_pattern {
-		"I[[:space:]]+$"	// The code I and no other fields.
-	};
+    // Since the requirements dictate that the formatting of the file is
+    // correct, these can be easily parsed with regualar expressions.
+    const std::regex inventory_pattern {
+        "I[[:space:]]+$"        // The action type I and no other fields
+    };
 
-	const std::regex history_pattern {
-		"H "			// The action type H
-		"([[:digit:]]{4})"	// The 4-digit customer id
-	};
+    const std::regex history_pattern {
+        "H "                    // The action type H
+        "([[:digit:]]{4})"      // The 4-digit customer id
+    };
 
-	const std::regex borrow_pattern {
-		"B "			// The action type B
-		"([[:digit:]]{4})"	// The 4-digit customer id
-	};
+    const std::regex borrow_pattern {
+        "B "                    // The action type B
+        "([[:digit:]]{4})"      // The 4-digit customer id
+    };
 
-	const std::regex return_pattern {
-		"R "			// The action type R
-		"([[:digit:]]{4})"	// The 4-digit customer id
-	};
+    const std::regex return_pattern {
+        "R "                    // The action type R
+        "([[:digit:]]{4})"      // The 4-digit customer id
+    };
 
-	// At this point all items are DVDs.
-	const std::regex comedy_pattern {
-		"D F "			// DVD & Movie type F
-		"([^,]+), "		// Title
-		"([[:digit:]]+)"	// Release Year
-	};
+    // At this point all items are DVDs. So, movie types are simply parsed
+    // as DVDs. If this system changes to accept move media types, this
+    // will need to be updated.
+    const std::regex comedy_pattern {
+        "D F "                  // DVD & Movie type F
+        "([^,]+), "             // Title
+        "([[:digit:]]+)"        // Release Year
+    };
 
-	const std::regex drama_pattern {
-		"D D "			// DVD & Movie type D
-		"([^,]+), "		// Director
-		"([^,\r]+)"		// Title
-	};
+    const std::regex drama_pattern {
+        "D D "                  // DVD & Movie type D
+        "([^,]+), "             // Director
+        "([^,\r]+)"             // Title
+    };
 
-	const std::regex classic_pattern {
-		"D C "			// Movie type C
-		"([[:digit:]]{1,2}) "	// Release month
-		"([[:digit:]]+) "	// Release year
-		"([^,\r]+)"		// Actor
-	};
+    const std::regex classic_pattern {
+        "D C "                  // DVD & Movie type C
+        "([[:digit:]]{1,2}) "   // Release month
+        "([[:digit:]]+) "       // Release year
+        "([^,\r]+)"             // Actor
+    };
 };
 
 #endif
