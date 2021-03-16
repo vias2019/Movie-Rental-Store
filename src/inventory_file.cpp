2951c9c66b91a0616902b6d247770e3f34e2da5d
Dominic Ewing <ewingdj@uw.edu>
2021-03-16 16:30:47 -0700

    Restyle InventoryFile

--- src/inventory_file.cpp
+++ src/inventory_file.cpp
@@ -3,112 +3,114 @@
  * @author Dominic Ewing
  */
 
-#include <fstream>
+#include <fstream>      // ifstream
 
 #include "inventory_file.h"
 
 /**
- * Construct an inventory file object.
+ * Constructor - Construct an inventory file object.
  *
  * @param filePath Path to the inventory file.
  */
 InventoryFile::InventoryFile(std::string filePath)
-	: file_path{std::move(filePath)}
+    : file_path{std::move(filePath)}
 {
 }
 
 /**
- * Create an inventory object representing the contents of the file.
+ * inventory - Create an inventory object representing the contents of the file.
  *
  * @return An inventory object representing the contents of the file.
  */
 Inventory InventoryFile::inventory()
 {
-	if (!parsed)
-	{
-		parseFile();
-	}
+    // Parse the file if there is no data in cache
+    if (!parsed)
+    {
+        parseFile();
+    }
 
-	return file_inventory;
+    return file_inventory;
 }
 
 /**
- * Provide all the errors encountered while parsing the file.
+ * errors - Provide all the errors encountered while parsing the file.
  *
  * @return A list of all parsing errors.
  */
 std::vector<std::shared_ptr<std::runtime_error>> InventoryFile::errors()
 {
-	if (!parsed)
-	{
-		parseFile();
-	}
+    // Parse the file if there is no data in cache
+    if (!parsed)
+    {
+        parseFile();
+    }
 
-	return error_list;
+    return error_list;
 }
 
 /**
- * Perform file parsing and cache the inventory and list of parsing errors.
+ * parseFile - Perform file parsing and cache the inventory and list of parsing
+ *             errors.
  */
 void InventoryFile::parseFile()
 {
-	std::ifstream stream{file_path};
+    std::ifstream stream{file_path};
 
-	for (std::string line; std::getline(stream, line); )
-	{
-		std::smatch comedy_match;
-		std::smatch drama_match;
-		std::smatch classic_match;
+    for (std::string line; std::getline(stream, line); )
+    {
+        std::smatch comedy_match;
+        std::smatch drama_match;
+        std::smatch classic_match;
 
-		if (std::regex_search(line, comedy_match, comedy_pattern))
-		{
-			file_inventory.addStock(
-				DVD(std::shared_ptr<Movie>{new ComedyMovie(
-					"comedy",		// .genre
-					comedy_match[2],	// .director
-					comedy_match[3],	// .title
-					stoi(comedy_match[4])	// .releaseYear
-					)}
-				),
-				stoi(comedy_match[1])		// .quantity
-			);
-		}
-		else if (std::regex_search(line, drama_match, drama_pattern))
-		{
-			file_inventory.addStock(
-				DVD(std::shared_ptr<Movie>{new DramaMovie(
-					"drama",		// .genre
-					drama_match[2],		// .director
-					drama_match[3],		// .title
-					stoi(drama_match[4])	// .releaseYear
-					)}
-				),
-				stoi(drama_match[1])		// .quantity
-			);
-		}
-		else if (std::regex_search(line, classic_match, classic_pattern))
-		{
-			file_inventory.addStock(
-				DVD(std::shared_ptr<Movie>{new ClassicMovie(
-					"classic",		// .genre
-					classic_match[2],	// .director
-					classic_match[3],	// .title
-					stoi(classic_match[6]),	// .releaseYear
-					stoi(classic_match[5]),	// .releaseMonth
-					classic_match[4]	// .actor
-					)}
-				),
-				stoi(classic_match[1])		// .quantity
-			);
-		}
-		else
-		{
-			error_list.emplace_back(
-				std::make_shared<std::runtime_error>(
-					"[Invalid inventory item] " + line
-			));
-		}
-	}
+        // Match based on movie type
+        if (std::regex_search(line, comedy_match, comedy_pattern))
+        {
+            file_inventory.addStock(
+                DVD(std::shared_ptr<Movie>{new ComedyMovie(
+                    "comedy",               // .genre
+                    comedy_match[2],        // .director
+                    comedy_match[3],        // .title
+                    stoi(comedy_match[4])   // .releaseYear
+                )}),
+                stoi(comedy_match[1])       // .quantity
+            );
+        }
+        else if (std::regex_search(line, drama_match, drama_pattern))
+        {
+            file_inventory.addStock(
+                DVD(std::shared_ptr<Movie>{new DramaMovie(
+                    "drama",                // .genre
+                    drama_match[2],         // .director
+                    drama_match[3],         // .title
+                    stoi(drama_match[4])    // .releaseYear
+                )}),
+                stoi(drama_match[1])        // .quantity
+            );
+        }
+        else if (std::regex_search(line, classic_match, classic_pattern))
+        {
+            file_inventory.addStock(
+                DVD(std::shared_ptr<Movie>{new ClassicMovie(
+                    "classic",              // .genre
+                    classic_match[2],       // .director
+                    classic_match[3],       // .title
+                    stoi(classic_match[6]), // .releaseYear
+                    stoi(classic_match[5]), // .releaseMonth
+                    classic_match[4]        // .actor
+                )}),
+                stoi(classic_match[1])      // .quantity
+            );
+        }
+        else
+        {
+            error_list.emplace_back(
+                std::make_shared<std::runtime_error>(
+                    "[Invalid inventory item] " + line
+            ));
+        }
+    }
 
-	parsed = true;
+    // Mark the cache as valid
+    parsed = true;
 }
--- src/inventory_file.h
+++ src/inventory_file.h
@@ -6,9 +6,9 @@
 #ifndef INVENTORY_FILE_H
 #define INVENTORY_FILE_H
 
-#include <regex>
-#include <string>
-#include <vector>
+#include <regex>    // regex, regex_search, smatch
+#include <string>   // string
+#include <vector>   // vector
 
 #include "inventory.h"
 
@@ -19,50 +19,52 @@
 class InventoryFile
 {
 public:
-	explicit InventoryFile(std::string filePath);
+    // Constructor
+    explicit InventoryFile(std::string filePath);
 
-	Inventory inventory();
-	std::vector<std::shared_ptr<std::runtime_error>> errors();
+    // Retrieve inventory and errors
+    Inventory inventory();
+    std::vector<std::shared_ptr<std::runtime_error>> errors();
 
 private:
-	std::string file_path;
+    std::string file_path;      // Path to the inventory file
 
-	// Cache from previously parsing the file. This prevents re-parsing
-	// for each separate call to inventory() and errors().
-	bool parsed{false};
-	Inventory file_inventory{};
-	std::vector<std::shared_ptr<std::runtime_error>> error_list{};
+    // Cache from previously parsing the file. This prevents re-parsing
+    // for each separate call to inventory() and errors().
+    bool parsed{false};
+    Inventory file_inventory{};
+    std::vector<std::shared_ptr<std::runtime_error>> error_list{};
 
-	// Helper method for perfoming file parsing and updating cache.
-	void parseFile();
+    // Helper method for perfoming file parsing and updating cache.
+    void parseFile();
 
-	// Since the assignment states that the formatting will be correct, parsing
-	// this with a regular expression should be ok.
-	const std::regex comedy_pattern {
-		"F, "				// F for comdedy
-		"([[:digit:]]+), "		// stock
-		"([^,]+), "			// director
-		"([^,]+), "			// title
-		"([[:digit:]]+)"		// year
-	};
+    // Since the requirements dictate that the formatting of the file is
+    // correct, these can be easily parsed with regualar expressions.
+    const std::regex comedy_pattern {
+        "F, "                   // F for comdedy
+        "([[:digit:]]+), "      // stock
+        "([^,]+), "             // director
+        "([^,]+), "             // title
+        "([[:digit:]]+)"        // year
+    };
 
-	const std::regex drama_pattern {
-		"D, "				// D for drama
-		"([[:digit:]]+), "		// stock
-		"([^,]+), " 			// director
-		"([^,]+), "			// title
-		"([[:digit:]]+)"		// year
-	};
+    const std::regex drama_pattern {
+        "D, "                   // D for drama
+        "([[:digit:]]+), "      // stock
+        "([^,]+), "             // director
+        "([^,]+), "             // title
+        "([[:digit:]]+)"        // year
+    };
 
-	const std::regex classic_pattern {
-		"C, "				// C for classic movie
-		"([[:digit:]]+), "		// stock
-		"([^,]+), "			// director
-		"([^,]+), "			// title
-		"([^, ]+ [^, ]+) "		// major actor
-		"([[:digit:]]{1,2}) "		// release month
-		"([[:digit:]]+)"		// release year
-	};
+    const std::regex classic_pattern {
+        "C, "                   // C for classic movie
+        "([[:digit:]]+), "      // stock
+        "([^,]+), "             // director
+        "([^,]+), "             // title
+        "([^, ]+ [^, ]+) "      // major actor
+        "([[:digit:]]{1,2}) "   // release month
+        "([[:digit:]]+)"        // release year
+    };
 };
 
 #endif
