b8799a4be5b9b418a4aed0cb2b849fa3e9341d25
Dominic Ewing <ewingdj@uw.edu>
2021-03-16 16:32:08 -0700

    Restyle main

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
-// Assignment4.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
+/**
+ * Assignment 4 - movie rental system
+ * This file provides the main method that drives the program.
+ *
+ * @file main.cpp
+ * @author Dominic Ewing
+ */
 
 #include "command_file.h"
 #include "console_display.h"
@@ -11,69 +14,60 @@
 #include "inventory_file.h"
 #include "rental_system.h"
 
-using namespace std;
+// Define the default input file locations
+#ifndef INVENTORY_FILE_LOCATION
+#define INVENTORY_FILE_LOCATION data4movies.txt
+#endif
 
+#ifndef CUSTOMER_FILE_LOCATION
+#define CUSTOMER_FILE_LOCATION data4customers.txt
+#endif
+
+#ifndef COMMAND_FILE_LOCATION
+#define COMMAND_FILE_LOCATION data4commands.txt
+#endif
+
+/**
+ * Main - create the system's main objects and drives the program
+ */
 int main()
 {
-	/*
-   //======================= Viktoriya's tests======================================================
-   CustomerFile Nova;
-   
-   HashTable M;
-   Nova.readFile("data4customers.txt", M);
-   M.insert(6, "Vika", "Gri");
-   M.insert(8, "Dasha", "Mo");
-   Customer v = M.findCustomer(3);
-   int size = M.getSize();
-   DVD movie1;
-   DVD movie2;
+    // Build the display.
+    ConsoleDisplay display{};
 
-   M.borrow(6, movie1);
-   M.borrow(6, movie2);
-  //M.restock(6, movie1);
-  // cout << "Print quantity: " << M.checkQuantity(6, movie1) << endl;
-   M.printTransactions(6);
- 
-   //M.deleteData(3);
-   //=================================================================================================
-   std::cout << "Hello World!\n";
-   */
+    // Build the inventory and report errors.
+    InventoryFile inventoryFile{"data/movies.txt"};
+    Inventory inventory = inventoryFile.inventory();
+    const auto inventoryErrors = inventoryFile.errors();
+    std::for_each(inventoryErrors.cbegin(), inventoryErrors.cend(),
+        [&display](const auto& error) {
+            display.displayError(*error);
+    });
 
-	// Build the display.
-	ConsoleDisplay display{};
+    // Build the client list.
+    CustomerFile customerFile;
+    HashTable customers;
+    customerFile.readFile("data/customers.txt", customers);
 
-	// Build the inventory and report errors.
-	InventoryFile inventoryFile{"data/movies.txt"};
-	Inventory inventory = inventoryFile.inventory();
-	const auto inventoryErrors = inventoryFile.errors();
-	std::for_each(inventoryErrors.cbegin(), inventoryErrors.cend(), [&display](const auto& error) {
-		display.displayError(*error);
-	});
+    // Build the rental system object.
+    RentalSystem rentalSystem{
+        inventory, customers, std::unique_ptr<Display>{new ConsoleDisplay{}}};
 
-	// Build the clientele and report errors.
-	// TODO report errors
-	CustomerFile customerFile;
-	HashTable customers;
-	customerFile.readFile("data/customers.txt", customers);
+    // Build the command list and report errors.
+    CommandFile command_file{"data/commands.txt"};
+    auto commands = command_file.commands();
+    const auto command_errors = command_file.errors();
+    std::for_each(command_errors.cbegin(), command_errors.cend(),
+        [&display](const auto& error) {
+            display.displayError(*error);
+    });
 
-	// Build the rental system object.
-	RentalSystem rentalSystem{inventory, customers, std::unique_ptr<Display>{new ConsoleDisplay{}}};
-
-	// Build the command list and report errors.
-	CommandFile command_file{"data/commands.txt"};
-	auto commands = command_file.commands();
-	const auto command_errors = command_file.errors();
-	std::for_each(command_errors.cbegin(), command_errors.cend(), [&display](const auto& error) {
-		display.displayError(*error);
-	});
-
-	// Run the commands.
-	std::for_each(commands.cbegin(), commands.cend(), [&rentalSystem](const auto& command) {
-		command->runWith(rentalSystem);
-	});
-	
-
-
-	return 0;
+    // Run the commands.
+    std::for_each(commands.cbegin(), commands.cend(),
+        [&rentalSystem](const auto& command) {
+            command->runWith(rentalSystem);
+    });
+    
+    return 0;
 }
 
